36a7e2659858c750253b898810fc8a67c7c588e4
senft-research <JordanSenft@proton.me>
2025-07-31 20:52:29 +0100

    Added Initial Sound logic for music (not going any further until separate branch)

--- Assets/_Scripts/Model/Collidables/Trash/TrashItem.cs
+++ Assets/_Scripts/Model/Collidables/Trash/TrashItem.cs
@@ -1,9 +1,16 @@
+using _Scripts.Util.Pools.Audio;
+using UnityEngine;
+
 namespace _Scripts.Model.Collidables.Trash
 {
     public class TrashItem : Collideable
     {
+        [SerializeField]
+        SoundData soundData;
+        
         protected override void OnCollide()
         {
+            SoundManager.Instance.CreateSoundBuilder().WithPosition(gameObject.transform.position).Play(soundData);
             Destroy(gameObject);
         }
     }
