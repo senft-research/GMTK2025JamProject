a9d66381fb709235cc4f398be1f731baf37b1856
ChroniclerDelta <TheChroniclerDelta@outlook.com>
2025-08-02 14:27:58 -0500

    New Pause Menu System - LevelCanvas, LevelObjectives, and LevelText have been implemented to set text they are attached to. - New LevelInfo, a new ScriptableObject that is meant to store the info and list of objectives for a level and to be sent to LevelCanvas

--- Assets/_Scripts/Model/Level/Canvas/LevelCanvas.cs
+++ Assets/_Scripts/Model/Level/Canvas/LevelCanvas.cs
@@ -1,14 +1,25 @@
+using System.Collections.Generic;
 using KBCore.Refs;
 using UnityEngine;
 
 namespace _Scripts.Model.Level.Canvas
 {
-    public class LevelCanvas : MonoBehaviour
+    public class LevelCanvas : ValidatedMonoBehaviour
     {
         [SerializeField, Child]
         LevelObjectives levelObjectives;
 
         [SerializeField, Child]
         LevelText levelText;
+
+        public void SetObjectives(List<string> objectives)
+        {
+            levelObjectives.SetObjectives(objectives);
+        }
+
+        public void SetLevelText(string text)
+        {
+            levelText.SetText(text);
+        }
     }
 }
--- Assets/_Scripts/Model/Level/Canvas/LevelObjectives.cs
+++ Assets/_Scripts/Model/Level/Canvas/LevelObjectives.cs
@@ -1,13 +1,30 @@
+using System;
 using System.Collections.Generic;
 using KBCore.Refs;
+using TMPro;
 
 namespace _Scripts.Model.Level.Canvas
 {
     public class LevelObjectives : ValidatedMonoBehaviour
     {
-        void SetObjectives(List<string> objectives)
+        private TMP_Text _text;
+        private string _stringToSend;
+
+        void Awake()
         {
-            
+            _text = gameObject.GetComponent<TMP_Text>();
+        }
+        
+        public void SetObjectives(List<string> objectives)
+        {
+            _stringToSend = String.Empty;
+            foreach (string objective in objectives)
+            {
+                _stringToSend += String.Format("• {0}\n", objective);
+            }
+
+            //_text.text = _stringToSend;
+            _text.SetText(_stringToSend);
         }
     }
 }
